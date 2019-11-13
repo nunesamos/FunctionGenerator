89774a3f1439c7845feea3223e8e3cd072be0105
gustavo61 <57459889+gustavo61@users.noreply.github.com>
2019-11-13 17:54:01 -0300

    Update PC3.c

--- PC3.c
+++ PC3.c
@@ -2,103 +2,229 @@
 
 #define pwm_out BIT0
 
-// Variaveis para PWM
+//condiguracao do display 7seg
+
+#define SEG_A   BIT0
+#define SEG_B   BIT1
+#define SEG_C   BIT2
+#define SEG_D   BIT3
+#define SEG_E   BIT4
+#define SEG_F   BIT5
+#define SEG_G   BIT6
+#define SEG_DP  BIT7
+
+#define D0  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_E + SEG_F)
+#define D1  (SEG_B + SEG_C)
+#define D2  (SEG_A + SEG_B + SEG_D + SEG_E + SEG_G)
+#define D3  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_G)
+#define D4  (SEG_B + SEG_C + SEG_F + SEG_G)
+#define D5  (SEG_A + SEG_C + SEG_D + SEG_F + SEG_G)
+#define D6  (SEG_A + SEG_C + SEG_D + SEG_E + SEG_F + SEG_G)
+#define D7  (SEG_A + SEG_B + SEG_C)
+#define D8  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_E + SEG_F + SEG_G)
+#define D9  (SEG_A + SEG_B + SEG_C + SEG_D + SEG_F + SEG_G)
+
+
+
+const unsigned int digits[10] = {D0, D1, D2, D3, D4, D5, D6, D7, D8, D9};
+
+//vetor de armazenamento para vizualizar a forma de onda
+
+int mpl=0;
+unsigned int memory [1000] = {0};
+unsigned int i;
+
+
 unsigned int duty=0;
 unsigned int frec=10000;
 
+int mem;
+
+//Vetores com valores pre-definidos de saída para gerar as formas de onda
+
+unsigned int sin[64] = {140, 152, 165, 176, 188, 198, 208, 218, 226, 234, 240, 245, 250, 253, 254, 255, 254, 253, 250, 245, 240, 234, 226, 218, 208,
+                        198, 188, 176, 165, 152, 140, 128, 115, 103, 90, 79, 67, 57, 47, 37, 29, 21, 15, 10, 5, 2, 1, 0, 1, 2, 5, 10, 15, 21, 29, 37,
+                        47, 57, 67, 79, 90, 103, 115, 127, } ;
+
+unsigned int tri[64] = {8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120, 128, 136, 144, 152, 160, 168, 176, 184, 192, 200, 208, 216, 224,
+                        232, 240, 248, 248, 240, 232, 224, 216, 208, 200, 192, 184, 176, 168, 160, 152, 144, 136, 128, 120, 112, 104, 96, 88, 80, 72, 64,
+                        56, 48, 40, 32, 24, 16, 8, 0, 0, };
+
+
+unsigned int fonda = 1;
+unsigned int range=1;
+
+unsigned int indice=0;
+
+unsigned int atual=0;
+
 // Variaveis para debounce
-int count=0,state;
+int count=0,state=0;
+int count2=0,state2=0;
 
 
 // Range de Frequencia
-int range=1;
 
-int main(void) {
+
+int main(void)
+{
     WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
 
-// -------------------------------- P W M ------------------------------------------
+    // Saida 7seg
 
-        P2DIR |= pwm_out;           // Atribui a direcao de saida do pwm do pino 2.0
-        P2SEL |= pwm_out;           // Seleciona a funcao especifica do pino
+    P3DIR = 0xFF;
+    P3OUT = 0x00;
 
-        TA1CCTL1 = OUTMOD_7;        // Modo de Reset/Set do timer
-        // Valor inicial do Timer para divisao do SMCLK (Modifica a frequencia)
-        TA1CCR0 = 10000;
-        TA1CCR1 = 0;                // Valor inicial para o Duty (Modifica o Duty Cyclo)
+    P2OUT = 0x00;
+    P2DIR |= BIT5 + BIT4;
 
-        TA1CTL = MC_1 | TASSEL_2;   // Conta no modo UP e seleciona o SMCLK
+    P4DIR |= BIT0 + BIT1;
 
+    // Configuracao do botao P2.1
 
-        volatile unsigned int i;
+    P2REN|=BIT1; //Ativa o resistor de pull/up
+    P2OUT|=BIT1; //Seta como resistor de pull/up
 
-        P6SEL |= BIT0 | BIT1;       // Habilita os canais de entrada analogica A0
-        // Reseta o REFMSTR. Valor de referencia dos potenciometros
-        REFCTL0 &= ~REFMSTR;
-
-        // Ativacao do conversor analogico-digital.
-        ADC12CTL0 = ADC12ON+ADC12MSC+ADC12SHT02+ADC12REFON+ADC12REF2_5V;
+    P2IE|=BIT1; //Ativa a interrupção para o botao
+    P2IES|=BIT1;
 
 
-        ADC12IE =  ADC12IE0 | ADC12IE1;      // Habilita as interrupcoes dos ADC's
-        ADC12CTL1 = ADC12SHP + ADC12CONSEQ_1;// Multiplos canais e habilita a amostragem
-        ADC12CTL2 = ADC12RES_0;              // Resolucao de 8 Bits
 
-        // Utiliza a referencia de 2,5v de entrada para o primeiro potenciometro
-        ADC12MCTL0 = ADC12SREF_1;
-        // Referencia de 2,5v para o segundo, seleciona o canal 6.1 de entrada.
-        ADC12MCTL1 = ADC12SREF_1 | ADC12INCH_1;
+    // Saida para o DAC
 
+    P1DIR |= BIT2+BIT3+BIT4+BIT5;
+    P1OUT &= ~(BIT2+BIT3+BIT4+BIT5);
 
-        for ( i=0; i<0x30; i++);             // Delay para inicializacao da referencia
+    P2DIR |= BIT5+BIT6;
+    P2OUT &= ~(BIT5+BIT6);
 
+    P2DIR |= pwm_out;           // Atribui a direcao de saida do pwm do pino 2.0
+    P2SEL |= pwm_out;           // Seleciona a funcao especifica do pino
 
-        ADC12CTL0 |= ADC12ENC | ADC12SC;     // Habilita as conversoes ADC.
+    // Configuracao Timer A1
 
+    TA1CCTL1 = OUTMOD_7;        // Modo de Reset/Set do timer
+    TA1CCR0 = 10000;
+    // Valor inicial do Timer para divisao do SMCLK (Modifica a frequencia)
+    TA1CCR1 = 0;                // Valor inicial para o Duty (Modifica o Duty Cyclo)
 
-// ------------------------------- D E B O U N C E -----------------------------------
-
-        P1DIR&=~BIT1; //Coloca P1.1 como saida
-        P1REN|=BIT1; //Ativa o resistor de pull/up
-        P1OUT|=BIT1; //Seta como resistor de pull/up
-
-        P1IE|=BIT1; //Ativa a interrupção para o botao
-        P1IES|=BIT1; //Coloca como borda de descida do botao
-
-        TA0CCR0=1000; //Contador de 0 a 1000  ~ 0.001 segundos
+    TA1CTL = MC_1 | TASSEL_2;   // Conta no modo UP e seleciona o SMCLK
 
 
-        __enable_interrupt(); //Habilita a interrupção
+    TA0CCR0 = 10000;
+    TA0CCTL0 = CCIE;// Valor inicial do Timer para divisao do SMCLK (Modifica a frequencia)
+    TA0CTL = MC_1 | TASSEL_2;   // Conta no modo UP e seleciona o SMCLK
 
-//      P1DIR|=BIT0; //P1.0 como saida
-//      P1OUT&=~BIT0; //Inicializa com o Led1 desligado
 
-//      P4DIR|=BIT7; //P4.7 como saida
+    volatile unsigned int i;
 
-// --------------------------------- Main Loop ---------------------------------------
+    //Configuracao ADC12
+
+    P6SEL |= BIT0 | BIT1;       // Habilita os canais de entrada analogica A0
+    REFCTL0 &= ~REFMSTR;
+    // Reseta o REFMSTR. Valor de referencia dos potenciometros
+
+    ADC12CTL0 = ADC12ON+ADC12MSC+ADC12SHT02+ADC12REFON+ADC12REF2_5V;
+    // Ativacao do conversor analogico-digital.
+
+    ADC12IE =  ADC12IE0 | ADC12IE1;      // Habilita as interrupcoes dos ADC's
+    ADC12CTL1 = ADC12SHP + ADC12CONSEQ_1;// Multiplos canais e habilita a amostragem
+    ADC12CTL2 = ADC12RES_0;              // Resolucao de 8 Bits
+
+    ADC12MCTL0 = ADC12SREF_1;
+    // Utiliza a referencia de 2,5v de entrada para o primeiro potenciometro
+    ADC12MCTL1 = ADC12SREF_1 | ADC12INCH_1;
+    // Referencia de 2,5v para o segundo, seleciona o canal 6.1 de entrada.
+
+    for ( i=0; i<0x30; i++);             // Delay para inicializacao da referencia
+
+
+    ADC12CTL0 |= ADC12ENC | ADC12SC;     // Habilita as conversoes ADC.
+
+    // Configuracao do botao P1.1
+
+    P1DIR&=~BIT1; //Coloca P1.1 como saida
+    P1REN|=BIT1; //Ativa o resistor de pull/up
+    P1OUT|=BIT1; //Seta como resistor de pull/up
+
+    P1IE|=BIT1; //Ativa a interrupção para o botao
+    P1IES|=BIT1; //Coloca como borda de descida do botao
+
+    // Configuracao Timer A0
+
+
+    TA0CCTL1 |= CCIE;
+    TA0CCTL2 |= CCIE;
+    TA0CCTL3 |= CCIE;
+    TA0CCTL4 |= CCIE;
+    TA0CCR1=1000;  //Contador de 0 a 1000  ~ 0.001 segundos
+    TA0CCR2=1000;
+    TA0CCR3=500;
+    TA0CCR4=5000;
+
+
+
+    __enable_interrupt();                // Habilita as interrupcoes
 
     while(1){
-        LPM1;                            //Low Power Mode 1
-//       P4OUT^=BIT7; //Toggle P4.7
-//       __delay_cycles(200000); //Delay
-   }
+        while (!(ADC12IFG & BIT0));
+        LPM0;                            //Low Power Mode 0
+}
 
 }
-//------------------------- Pragma Interrupt para o Debounce -----------------------------
+#pragma vector=ADC12_VECTOR
+    __interrupt void OndaQuadrada (void){
+        switch (__even_in_range(ADC12IV,8)) // Caracteriza prioridades de interrupcoes.
+        {
+            case  0: break;                           // Vector  0:  No interrupt
+            case  2: break;                           // Vector  2:  ADC overflow
+            case  4: break;                           // Vector  4:  ADC timing overflow
+            case  6:
 
-#pragma vector=PORT1_VECTOR
-__interrupt void P1_Function()
-{
-    count=0; //Reset count
-    TA0CTL|=TASSEL_2+MC_1+TAIE; //Timer0 recebe ACLK
-    state=(P1IN&BIT1); //Armazena o valor do Botao
-    P1IE&=~BIT1; //Desabilita a interrupcao
-    P1IFG&=~BIT1; // Reset Port1 interrupt flag
+                mem = ADC12MEM0;
+
+               duty = (frec/2);
+               // Atribui a variacao do potenciometro para a variavel Duty
+               TA1CCR1 = duty;              // Atribui o valor de duty para o Timer 1
+               ADC12CTL0 |= ADC12SC;        // Habiita novamente as conversoes
+            break;
+
+            case  8:
+
+               frec = 10000 - (10000/250)*ADC12MEM1;
+               // Atribui a variacao do potenciometro para a variavel frec
+               TA1CCR0 = frec;              // Atribui o valor de frec para o Timer 2
+               ADC12CTL0 |= ADC12SC;        // Habiita novamente as conversoes
+            break;
+            case 10: break;                           // Vector 10:  ADC12IFG2
+            case 12: break;                           // Vector 12:  ADC12IFG3
+            case 14: break;                           // Vector 14:  ADC12IFG4
+            case 16: break;                           // Vector 16:  ADC12IFG5
+            case 18: break;                           // Vector 18:  ADC12IFG6
+            case 20: break;                           // Vector 20:  ADC12IFG7
+            case 22: break;                           // Vector 22:  ADC12IFG8
+            case 24: break;                           // Vector 24:  ADC12IFG9
+            case 26: break;                           // Vector 26:  ADC12IFG10
+            case 28: break;                           // Vector 28:  ADC12IFG11
+            case 30: break;                           // Vector 30:  ADC12IFG12
+            case 32: break;                           // Vector 32:  ADC12IFG13
+            case 34: break;                           // Vector 34:  ADC12IFG14
+            default: break;
+        }
+
+
 }
 
 #pragma vector=TIMER0_A1_VECTOR
 __interrupt void TMR0()
 {
 
+    switch( TA0IV ){
+
+
+    case 2: //debounce B1
+
+            __delay_cycles(1000);
             if(state==(P1IN&BIT1)) {
                 count++; //incrementa se permanecer a mesma entrada
             } else
@@ -107,9 +233,8 @@ __interrupt void TMR0()
                 state=((P1IN&BIT1));
             }
 
-        if(count==10){ //Conta x10 o valor atribuido ao timer
+        if(count==5){ //Conta x10 o valor atribuido ao timer
             if(state==0){//Botao precionado
-//              P1OUT^=BIT0; //Toggle the LED
                 if(range==5){
                     range=1;
                 } else{
@@ -117,40 +242,87 @@ __interrupt void TMR0()
                 }
             }
             P1IE|=BIT1; //Ativa novamente a interrupcao
-            TA0CTL=0; //para o timer
-            TA0CTL|=TACLR; //Clear the Timer counter
+
             }
 
             TA0CTL&=~(TAIFG); //Reseta a interrupcao
+
+    case 4: i++ ;
+            if (i==1000) i = 0;
+
+            memory[i] = atual;
+
+
+    case 6: //debounce B2
+        __delay_cycles(1000);
+        if(state2==(P2IN&BIT1)) {
+                        count2++; //incrementa se permanecer a mesma entrada
+                    } else
+                    {
+                        count2=0; //se nao reseta o contador
+                        state=((P2IN&BIT1));
+                    }
+
+                if(count2==10){ //Conta x10 o valor atribuido ao timer
+                    if(state2==0){//Botao precionado
+
+                        if(fonda==2){
+                            fonda=1;
+                        } else{
+                            fonda++;
+                        }
+                    }
+                    P2IE|=BIT1; //Ativa novamente a interrupcao
+
+                    }
+
+                    TA0CTL&=~(TAIFG); //Reseta a interrupcao
+
+    case 8: //multiplexacao Displays 7seg
+        if (mpl == 1){
+            mpl = 0;
+            P4OUT = 1;
+            P3OUT = digits[range];
+
+        }
+
+        else {
+            mpl=1;
+            P4OUT = 2;
+            P3OUT = digits[fonda];
+        }
+
+    default: break;}
+}
+
+//incrementacao do indice para a mundança de saida a fim de gerar a forma de onda
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A0 (void)
+{
+
+    if (fonda == 1) {atual = sin[indice];}
+    else {atual = tri[indice];}
+
+
+   if (indice == 63 ) {indice = 0;}
+   else {indice++;}
 }
 
 
-//------------------------- Pragma Interrupt para o PWM -----------------------------
+#pragma vector=PORT1_VECTOR
+__interrupt void P1_Function()
+{
+    count=0; //Reset count
+    state=(P1IN&BIT1); //Armazena o valor do Botao
+    P1IE&=~BIT1; //Desabilita a interrupcao
+    P1IFG&=~BIT1; // Reset Port1 interrupt flag
+}
 
-#pragma vector=ADC12_VECTOR
-    __interrupt void OndaQuadrada (void){
-        switch (__even_in_range(ADC12IV,8)) // Caracteriza prioridades de interrupcoes.
-        {
-           case 0x06:
-               // Atribui a variacao do potenciometro para a variavel Duty
-               duty = (frec/250)*ADC12MEM0;
-               TA1CCR1 = duty;              // Atribui o valor de duty para o Timer 1
-               ADC12CTL0 |= ADC12SC;        // Habiita novamente as conversoes
-//               ADC12IFG &=~(ADC12IFG0);           // Reseta a flag de interrupcao
-           break;
-
-           case 0x08:
-               // Atribui a variacao do potenciometro para a variavel frec
-               frec = (10000 - (10000/250)*ADC12MEM1)/range;
-               TA1CCR0 = frec;              // Atribui o valor de frec para o Timer 2
-               ADC12CTL0 |= ADC12SC;        // Habiita novamente as conversoes
-//               ADC12IFG &=~(ADC12IFG1);           // Reseta a flag de interrupcao
-           break;
-
-           default:
-           break;
-        }
-        ADC12IFG&=~(ADC12IFG0);           // Reseta a flag de interrupcao
-        ADC12IFG&=~(ADC12IFG1);           // Reseta a flag de interrupcao
-    }
-
+#pragma vector=PORT2_VECTOR
+__interrupt void P2_Function()
+{
+    count2=0; //Reset count
+    state2=(P2IN&BIT1); //Armazena o valor do Botao
+    P2IE&=~BIT1; //Desabilita a interrupcao
+    P2IFG&=~BIT1; // Reset Port2 interrupt flag
+}
